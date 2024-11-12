272ea82d7fb1e20881e964d42a20c8e6e719861d
vivasuk683 <vivas@DESKTOP-BE431TS>
2024-11-12 21:49:10 +0300

    Сортировка пузырьком, добавлена проверка на отсортированный массив

--- Задача2/Задача2/main2.cpp
+++ Задача2/Задача2/main2.cpp
@@ -11,10 +11,15 @@ int main() {
 
 	// 5 7 8 2 3
 	for (int g = 0; g < 15; g++) {
+		bool F = false;
 		for (int i = 0; i < 15; i++) {
 			if (array[i] < array[i + 1]) {
 				swap(array[i], array[i + 1]);
+				F = true;
 			}
+		}
+		if (!F) {
+			break;
 		}
 	}
 	for (int i = 0; i < 15; i++) {
