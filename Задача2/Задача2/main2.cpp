20b5bf39aab7bbe26a7b9f9e1c02a270f0e05d23
vivasuk683 <vivas@DESKTOP-BE431TS>
2024-11-12 21:41:52 +0300

    коктельная сортировка незаконченая

--- Задача2/Задача2/main2.cpp
+++ Задача2/Задача2/main2.cpp
@@ -2,22 +2,27 @@
 using namespace std;
 
 int main() {
-	float array[15];
-	for (int i = 0; i < 15; i++) {
+	float array[5];
+	for (int i = 0; i < 5; i++) {
 		cout << "Input " << i + 1 << " array number";
 		cin >> array[i];
 	}
 	cout << "Your array:\n";
 
 	// 5 7 8 2 3
-	for (int g = 0; g < 15; g++) {
-		for (int i = 0; i < 15; i++) {
+	for (int g = 0; g < 5; g++) {
+		for (int i = 0; i < 5; i++) {
 			if (array[i] < array[i + 1]) {
 				swap(array[i], array[i + 1]);
 			}
 		}
+		for (int h = 4; h > 0; h--) {
+			if (array[h] > array[h - 1]) {
+				swap(array[h], array[h - 1]);
+			}
+		}
 	}
-	for (int i = 0; i < 15; i++) {
+	for (int i = 0; i < 5; i++) {
 		cout<< i + 1 << ". " << array[i] << "\n";
 	}
 }
