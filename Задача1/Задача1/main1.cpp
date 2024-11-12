1f037d6bd1424c0b2e0a9a0c958e3db69dddf944
vivasuk683 <vivas@DESKTOP-BE431TS>
2024-11-12 19:50:19 +0300

    Добавьте файлы проекта.

---
+++ Задача1/Задача1/main1.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	string surname[10];
+	for (int i = 1; i <= 10; ++i) {
+		cout << "Input surname apartment tenant ¹ " << i << " :";
+		cin >> surname[i - 1];
+	}
+	for (int i = 0; i < 3; i++) {
+		int a = 0;
+		cout << "Input apartment number: ";
+		cin >> a;
+		if (a > 0 && a <= 10) {
+			cout << surname[a - 1] << "\n";
+		}
+		else {
+			cout << "Invalid number\n";
+		}
+	}
+
+}
