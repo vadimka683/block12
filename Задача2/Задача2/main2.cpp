279af925409ff8337b8c607a9ad2989117305272
vivasuk683 <vivas@DESKTOP-BE431TS>
2024-11-12 22:40:40 +0300

    коктельная сортировка готово

--- Задача2/Задача2/main2.cpp
+++ Задача2/Задача2/main2.cpp
@@ -2,27 +2,41 @@
 using namespace std;
 
 int main() {
-	float array[5];
-	for (int i = 0; i < 5; i++) {
+	float array[15];
+	for (int i = 0; i < 15; i++) {
 		cout << "Input " << i + 1 << " array number";
 		cin >> array[i];
 	}
 	cout << "Your array:\n";
-
-	// 5 7 8 2 3
-	for (int g = 0; g < 5; g++) {
-		for (int i = 0; i < 5; i++) {
-			if (array[i] < array[i + 1]) {
-				swap(array[i], array[i + 1]);
+	int left_Mark = 0;
+	int Right_Marck = 15;
+	bool F = false;
+	int count = 0;
+	for (int g = 0; left_Mark < Right_Marck; g++) {
+		count++;
+		if (!F) {
+			for (int i = 0; i < Right_Marck; i++) {
+				if (array[i] < array[i + 1]) {
+					swap(array[i], array[i + 1]);
+				}
+				count++;
 			}
+			Right_Marck--;
+			F = true;
 		}
-		for (int h = 4; h > 0; h--) {
-			if (array[h] > array[h - 1]) {
-				swap(array[h], array[h - 1]);
+		else {
+			for (int h = 15; h > left_Mark; h--) {
+				if (array[h] > array[h - 1]) {
+					swap(array[h], array[h - 1]);
+				}
+				count++;
 			}
+			left_Mark++;
+			F = false;
 		}
 	}
-	for (int i = 0; i < 5; i++) {
+	cout << count;
+	for (int i = 0; i < 15; i++) {
 		cout<< i + 1 << ". " << array[i] << "\n";
 	}
 }
