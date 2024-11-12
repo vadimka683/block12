cd7338463841119925458439e81e31bbf0155200
vivasuk683 <vivas@DESKTOP-BE431TS>
2024-11-12 21:03:52 +0300

    сортировка пузырьком

--- Задача2/Задача2/Source.cpp
+++ Задача2/Задача2/Source.cpp
@@ -1,2 +1,23 @@
 #include<iostream>
 using namespace std;
+
+int main() {
+	float array[15];
+	for (int i = 0; i < 15; i++) {
+		cout << "Input " << i + 1 << " array number";
+		cin >> array[i];
+	}
+	cout << "Your array:\n";
+
+	// 5 7 8 2 3
+	for (int g = 0; g < 15; g++) {
+		for (int i = 0; i < 15; i++) {
+			if (array[i] < array[i + 1]) {
+				swap(array[i], array[i + 1]);
+			}
+		}
+	}
+	for (int i = 0; i < 15; i++) {
+		cout<< i + 1 << ". " << array[i] << "\n";
+	}
+}
