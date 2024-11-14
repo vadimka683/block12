0570858271746dfd7851f787a742222f5394f34b
vivasuk683 <vivas@DESKTOP-BE431TS>
2024-11-14 15:05:56 +0300

    сортировка вставкой в новый массив, работа с массивами в функциях, через указатели

--- Задача2/Задача2/main2.cpp
+++ Задача2/Задача2/main2.cpp
@@ -1,42 +1,45 @@
 #include<iostream>
 using namespace std;
 
+float* input_array(float *array){
+	for (int i = 0; i < 15; i++) {
+		float a;
+		cout << "input " << i + 1 << " number: ";
+		cin >> a;
+		*array = a;
+		array++;
+	}
+	return array -= 15;
+}
+void read_array(float* array) {
+	for (int i = 0; i < 15; i++) {
+		cout << i + 1 << "\t" << *array++ << "\n";
+	}
+	return;
+}
+float* sort_array(float* array, float *new_array_flag) {
+	float max = 0;
+	int number_max = 0;
+	for (int g = 0; g < 15; g++) {
+		for (int i = 0; i < 15; i++) {
+			if (array[i] > max) {
+				max = array[i];
+				number_max = i;
+			}
+		}
+		array[number_max] = 0;
+		*new_array_flag++ = max;
+		max = 0;
+	}
+	return new_array_flag -= 15;
+}
 int main() {
-	float array[15];
-	for (int i = 0; i < 15; i++) {
-		cout << "Input " << i + 1 << " array number";
-		cin >> array[i];
-	}
-	cout << "Your array:\n"; 
+	float array_base[15];
+	float* array = array_base;
+	array = input_array(array);
 
-	int count = 0;
-	float lenght_array = 14;
-
-	while (lenght_array > 1) {
-		for (int g = 0; g+lenght_array < 15; g++) {
-			if (array[g] < array[g + (int)lenght_array]) {
-				swap(array[g], array[g + (int)lenght_array]);
-			}
-			count++;
-		}
-		lenght_array /= 1.247;
-	}
-	for (int i = 0; i < 15; i++) {
-		bool F = false;
-		count++;
-		for (int g = 0; g < 15; g++) {
-			if (array[g] < array[g + 1]) {
-				swap(array[g], array[g + 1]);
-				F = true;
-			}
-			count++;
-		}
-		if (!F) {
-			break;
-		}
-	}
-	cout << count;
-	for (int i = 0; i < 15; i++) {
-		cout<< i + 1 << ". " << array[i] << "\n";
-	}
+	float new_array[15];
+	float* new_array_flag = &new_array[0];
+	new_array_flag = sort_array(array,new_array_flag);
+	read_array(new_array_flag);
 }
